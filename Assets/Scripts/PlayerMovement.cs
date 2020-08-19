dbf28d23b1f9acc52e82138ac736fc7c36b1a904
gdaugherty <garyd@undertonefx.com>
2020-08-19 10:31:00 -0700

    Greyboxing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HorMove = Input.GetAxis("Horizontal");
+
+        rbPlayer.AddForce(new Vector3(HorMove, 0, 0), ForceMode.Impulse);
+    }
+}
