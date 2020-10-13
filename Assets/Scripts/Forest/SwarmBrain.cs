0c81f913ce74ffdc27d97ce0b9930b93477ad1fa
gdaugherty <garyd@undertonefx.com>
2020-10-12 22:11:30 -0700

    Added AI

---
+++ Assets/Scripts/Forest/SwarmBrain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmBrain : MonoBehaviour
+{
+    // While it has the hive, patrol around it
+    // When no hive, will pursue the player
+    private bool hasHive = true;
+    private Patrol patrol;
+    private Bot bot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        patrol = GetComponent<Patrol>();
+        bot = GetComponent<Bot>();
+        HivePickUp.HivePickedUp += HiveTaken;
+    }
+
+    void HiveTaken()
+    {
+        hasHive = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasHive)
+        {
+            patrol.PatrolWaypoints();
+        }
+        else
+        {
+            bot.Pursue();
+        }
+    }
+}
